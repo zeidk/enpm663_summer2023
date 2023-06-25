7fba8389e3f94f65e154ae344065c3f640bd9e94
zeidk <zeidk@umd.edu>
2023-06-25 16:38:55 -0400

    launch files

--- first_package/launch/first_package_1.launch.py
+++ first_package/launch/first_package_1.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 
 # this function is needed
 def generate_launch_description():
-    ld = LaunchDescription()                # instantiate a Launchdescription object
+                    # instantiate a Launchdescription object
     publisher_node = Node(                  # declare your Node
         package="first_package",            # package name
         executable="advanced_publisher.py"  # executable as set in setup.py
@@ -13,6 +13,8 @@ def generate_launch_description():
         package="first_package",
         executable="subscriber_exe"
     )
+    
+    ld = LaunchDescription()
     ld.add_action(publisher_node)   # add each Node to the LaunchDescription object
     ld.add_action(subscriber_node)  # add each Node to the LaunchDescription object
     return ld                       # return the LaunchDescription object
--- first_package/launch/first_package_2.launch.py
+++ first_package/launch/first_package_2.launch.py
@@ -3,13 +3,11 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="first_package",
-            executable="advanced_publisher.py",
-        ),
-        Node(
-            package="first_package",
-            executable="subscriber_exe",
-        ),
-    ])
+    node1 = Node(package="first_package",
+                 executable="advanced_publisher.py",
+                 )
+    node2 = Node(
+        package="first_package",
+        executable="subscriber_exe",
+    )
+    return LaunchDescription([node1, node2])
