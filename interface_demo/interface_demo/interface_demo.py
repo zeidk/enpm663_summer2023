3c19ce1a222cd0bd8235cb7c2eb49948eddc00aa
zeidk <zeidk@umd.edu>
2023-06-28 01:24:44 -0400

    python service

--- interface_demo/interface_demo/interface_demo.py
+++ interface_demo/interface_demo/interface_demo.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 import random
 from rclpy.node import Node
-import rclpy
 from builtin_interfaces.msg import Time
 from enpm663_msgs.msg import WeatherStation
 from enpm663_msgs.srv import AddTwoInts
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 
 class Weather(Node):
@@ -33,23 +33,44 @@ class Weather(Node):
 
 
 class AddTwoIntsClient(Node):
+    '''
+    Class to call the add_two_ints service
+    '''
+
     def __init__(self, node_name):
         super().__init__(node_name)
-        self._client = self.create_client(AddTwoInts, 'add_two_ints')
-        while not self._client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
+        # Create various callback groups to ensure that various events are called in a mutually exclusive manner
+        client_cb_group = MutuallyExclusiveCallbackGroup()
+        timer_cb_group = MutuallyExclusiveCallbackGroup()
 
+        self._client = self.create_client(AddTwoInts, 'add_two_ints', callback_group=client_cb_group)
+
+        # Wait for the service to be available
+        while not self._client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting...')
+
+        self._logger.info("client created")
         self._request = AddTwoInts.Request()
-        self._client_timer = self.create_timer(2, self._client_timer_cb)
+        self._client_timer = self.create_timer(2, self._client_timer_cb, callback_group=timer_cb_group)
 
     def _client_timer_cb(self):
+        '''
+        Callback function for the client timer
+        The body of this function is executed every 2 seconds
+        '''
         a = random.randint(0, 9)
         b = random.randint(0, 9)
         self.send_request(a, b)
 
     def send_request(self, a, b):
+        '''
+        Send a request to the add_two_ints service
+
+        Args:
+            a (int): First integer
+            b (int): Second integer
+        '''
         self._request.a = a
         self._request.b = b
-        future = self._client.call_async(self._request)
-        rclpy.spin_until_future_complete(self, future)
-        return future.result()
+        response = self._client.call(self._request)
+        self.get_logger().info(f'Response: {response.sum}')
